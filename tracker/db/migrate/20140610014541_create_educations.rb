ca7d66e6c2b0ee1ec8b58d452fb602942562d40d
Alan Haikal <alanhaikal@gmail.com>
2014-06-09 22:47:53 -0400

    added migration and switched to ruby 211

--- tracker/db/migrate/20140610014541_create_educations.rb
+++ tracker/db/migrate/20140610014541_create_educations.rb
@@ -1,6 +1,13 @@
 class CreateEducations < ActiveRecord::Migration
   def change
     create_table :educations do |t|
+    	t.string :name
+    	t.integer :duration
+    	t.date :date
+    	t.string :location
+    	t.text :description
+    	t.boolean :attended
+    	t.date :date_completed
 
       t.timestamps
     end
