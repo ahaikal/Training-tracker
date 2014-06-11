bd0b82aaf1f8b305c3e891a502d2c17b37650782
Alan Haikal <alanhaikal@gmail.com>
2014-06-11 15:05:23 -0400

    add association

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,14 @@
 class Admin < ActiveRecord::Base
+	has_secure_password
+
+	has_many :departments
+	belongs_to :hospital
+
+	validates_presence_of :name
+  validates_presence_of :email
+
+
+
+
+
 end
